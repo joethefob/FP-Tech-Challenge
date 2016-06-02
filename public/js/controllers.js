d4b66152ad81f0ef4b1a415ee125e0f96e9b3064
Weichen Zhou <weichenzhou94@gmail.com>
2016-06-02 18:13:07 -0400

    Whenever a style code is selected, its corresponding colour and size codes appear

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,21 +3,27 @@
 /* Controller */
 angular.module('myApp.controllers', [])
 	.controller('AppCtrl', function($scope, $http) {
-    // $scope.data = {
-    //  repeatSelect: null,
-    //  availableOptions: [
-    //    { style_code: '1', name: 'Option A'},
-    //    {id: '2', name: 'Option B'},
-    //    {id: '3', name: 'Option D'}
-    //  ],
-    //  test: "hello"
-
         $scope.data = {
+            availableStyles: null,
+            availableColours: null,
+            availableSizes: null,
+
             selectedStyle: null,
-            availableStyles: null
+            selectedColour: null,
+            selectedSize: null,
         };
 
     	$http.get("/api/apparel").then(function(response) {
             $scope.data.availableStyles = response.data;
     	});
+
+        $scope.$watch('data.selectedStyle', function() {
+            $http.get("/api/apparel/" + $scope.data.selectedStyle).then(function(response) {
+                $scope.data.availableColours = response.data.colours;
+                $scope.data.availableSizes = response.data.sizes;
+            });
+
+            $scope.data.selectedColour = null;
+            $scope.data.selectedSize = null;
+        });
     });
--- routes/api.js
+++ routes/api.js
@@ -7,19 +7,56 @@ var q           = require('q');
 var api         = express.Router();
 var db          = require('../config/db').connection;
 
+function parseColourCodes(colour_code_str) {
+	var colours = [];
+	var colour_codes = colour_code_str.split(';');
+	for (var i = 0; i < colour_codes.length; ++i) {
+		var colour = colour_codes[i].split(':');
+		colours.push({ "colour_code": colour[0], "RGB": colour[1], "name": colour[2] });
+	}
+
+	return colours;
+}
+
+function parseSizeCodes(size_code_str) {
+	var sizes = [];
+	var size_codes = size_code_str.split(';');
+	for (var i = 0; i < size_codes.length; ++i) {
+		var size = size_codes[i].split(':');
+		sizes.push({ "size_code": size[0], "size": size[1] });
+	}
+
+	return sizes;
+}
+
 // API endpoint for /api/apparel
 api.get('/api/apparel/:styleCode?', function(req, res) {
 	var styleCode = req.params["styleCode"];
 
+	// If styleCode is undefined, send back a list of style codes
 	if (typeof styleCode === "undefined") {
-		db.query('SELECT id, brand, style_code FROM apparel', function(err, rows, fields) {
+		db.query('SELECT brand, style_code FROM apparel', function(err, rows, fields) {
 			if (err) throw err;
 			res.send(rows);
 		});
+	} 
+	// Otherwise, send back a list of colour codes and size codes
+	else {
+		db.query("SELECT color_codes, size_codes FROM apparel WHERE apparel.style_code='" + styleCode + "'", function(err, rows, fields) {
+			if (err) throw err;
+
+			if (rows.length < 1) {
+				console.log("Style Code not found!");
+				res.send(null);
+			} else {
+				res.send({
+					"colours": parseColourCodes(rows[0].color_codes),
+					"sizes": parseSizeCodes(rows[0].size_codes)
+				});
+			}
+		});
 	}
 });
-
-// API endpoint for 
 
 // API endpoint for /api/quote
 api.post('/api/quote', function(req, res) {
